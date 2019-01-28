529e4f90e8a9b9bab2fb17147cd3eec48ebc9cb1
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-01-28 13:47:46 +0200

    Add test for publish plan command

---
+++ spec/commands/decidim/plans/publish_plan_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Plans::PublishPlan do
+  let(:component) { create(:plan_component) }
+  let(:organization) { component.organization }
+  let(:state) { :unpublished }
+  let(:author) { create :user, :confirmed, organization: organization }
+  let(:non_author) { create :user, :confirmed, organization: organization }
+  let(:plan) { create(:plan, state, component: component, users: [author]) }
+
+  describe "call" do
+    describe "when called by the author" do
+      let(:subject) do
+        described_class.new(plan, author)
+      end
+
+      it "broadcasts ok" do
+        expect { subject.call }.to broadcast(:ok)
+        expect(plan.published_at).not_to be_nil
+      end
+    end
+
+    describe "when called by non-author" do
+      let(:subject) do
+        described_class.new(plan, non_author)
+      end
+
+      it "broadcasts invalid" do
+        expect { subject.call }.to broadcast(:invalid)
+      end
+    end
+  end
+end
