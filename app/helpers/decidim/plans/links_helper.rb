0de92a3fff282cb2e0f73ef108834dc023a1e7b3
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-03-07 09:11:39 +0200

    Keep random seed in the URL when browsing the plans list

--- app/helpers/decidim/plans/links_helper.rb
+++ app/helpers/decidim/plans/links_helper.rb
@@ -14,13 +14,31 @@ module Decidim
             :participatory_process_slug,
             :id
           ])
-        ).merge(extra_params)
+        ).merge(prepare_extra_params(extra_params))
       end
 
       def request_params_query(extra_params={}, exclude_params=[])
         return "" unless request_params(extra_params, exclude_params).any?
 
         "?#{request_params.to_query}"
+      end
+
+      private
+
+      # Adds the random seed to the extra parameters to maintain the ordering
+      # correctly across the requests.
+      def prepare_extra_params(extra_params)
+        return extra_params unless controller
+        return extra_params unless controller.respond_to?(:order, true)
+        return extra_params unless controller.respond_to?(:random_seed, true)
+
+        order = controller.send(:order)
+        return extra_params unless order == "random"
+
+        seed = controller.send(:random_seed)
+        return extra_params unless seed
+
+        extra_params.merge(random_seed: seed)
       end
     end
   end
