6193245647ba2a1a5c459d6fbde5beacbfa535d1
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2019-12-04 13:58:31 +0200

    Fix deprecation warning regarding papertrail

--- app/models/concerns/decidim/plans/traceable.rb
+++ app/models/concerns/decidim/plans/traceable.rb
@@ -22,7 +22,7 @@ module Decidim
         # This is customized with the `:ignore` option since we don't want to
         # store another version when the item is published.
         has_paper_trail ignore: [:published_at],
-                        class_name: "Decidim::Plans::PaperTrail::Version"
+                        versions: { class_name: "Decidim::Plans::PaperTrail::Version" }
 
         delegate :count, to: :versions, prefix: true
 
@@ -41,6 +41,7 @@ module Decidim
       # functionality.
       def is_a?(klass)
         return true if klass == Decidim::Traceable
+
         super
       end
     end
