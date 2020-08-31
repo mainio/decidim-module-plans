1715593d75b6593c01460258318c4e37ea3086a1
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-08-31 09:43:28 +0300

    Add default column value in old migration

--- db/migrate/20181230111931_create_decidim_plans_plan_contents.rb
+++ db/migrate/20181230111931_create_decidim_plans_plan_contents.rb
@@ -3,7 +3,7 @@
 class CreateDecidimPlansPlanContents < ActiveRecord::Migration[5.2]
   def change
     create_table :decidim_plans_plan_contents do |t|
-      t.jsonb :body, default: []
+      t.jsonb :body, default: {}
       t.references :decidim_user, index: true
       t.references :decidim_plan, index: true
       t.references :decidim_section, index: { name: "index_decidim_plans_contents_section_id" }
