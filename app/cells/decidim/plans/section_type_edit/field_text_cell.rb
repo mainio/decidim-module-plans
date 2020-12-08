6fecb775cc1b9bad22ca4aa7f4cc95c11e664bb0
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2020-12-08 16:03:49 +0200

    Fix required fields and hide Decidim core character counter

--- app/cells/decidim/plans/section_type_edit/field_text_cell.rb
+++ app/cells/decidim/plans/section_type_edit/field_text_cell.rb
@@ -10,7 +10,12 @@ module Decidim
           base = super
           return base if model.section.settings["answer_length"].to_i < 1
 
-          base.merge(label: false, maxlength: model.section.settings["answer_length"])
+          base.merge(
+            label: false,
+            maxlength: model.section.settings["answer_length"],
+            # Disable the core character counter with a hidden dummy element
+            "data-remaining-characters": "#plans-dummy-counter-#{section.id}"
+          )
         end
       end
     end
