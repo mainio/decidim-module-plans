700888fc5385ee1b2f7c1165b6fb81da23c96ede
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2021-01-12 14:03:13 +0200

    Fix sub-category filtering

--- app/services/decidim/plans/section_control/category.rb
+++ app/services/decidim/plans/section_control/category.rb
@@ -10,9 +10,12 @@ module Decidim
           return query if params["category_id"].blank?
 
           ref = Arel.sql("plan_content_#{section.id}")
+          ref_cats = Arel.sql("plan_categories_#{section.id}")
           query.joins(
             "LEFT JOIN decidim_plans_plan_contents AS #{ref} ON #{ref}.decidim_plan_id = #{Arel.sql(query.table_name)}.id"
-          ).where("#{ref}.body->>'category_id' =?", params["category_id"])
+          ).joins(
+            "LEFT JOIN decidim_categories AS #{ref_cats} ON #{ref_cats}.id = CAST(coalesce(#{ref}.body->>'category_id', '0') AS integer)"
+          ).where("#{ref_cats}.id =? OR #{ref_cats}.parent_id =?", params["category_id"], params["category_id"])
         end
 
         def search_params_for(_section)
