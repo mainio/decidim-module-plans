42ff102bc9bfa86b60c4f6f77c7d7b5ebb431eb8
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-06-26 20:03:03 +0300

    Update sections spec correct order

--- spec/commands/decidim/plans/admin/update_sections_spec.rb
+++ spec/commands/decidim/plans/admin/update_sections_spec.rb
@@ -9,7 +9,7 @@ module Decidim
         let(:current_organization) { create(:organization) }
         let(:participatory_process) { create(:participatory_process, organization: current_organization) }
         let(:sections_component) { create(:dummy_component) }
-        let(:sections) { Section.where(component: sections_component) }
+        let(:sections) { Section.where(component: sections_component).order(:position) }
         let(:form_params) do
           {
             "sections" => {
