cab72b6cf6a91d4780615ca18a0c8ad52e49cd93
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2023-07-05 01:31:29 +0300

    Correct model name

--- app/models/decidim/plans/section_view_block.rb
+++ app/models/decidim/plans/section_view_block.rb
@@ -5,7 +5,7 @@ module Decidim
     # A plan section view is a record that allows the admin user to customize
     # how the section is displayed. The section can be split into sub-views that
     # display a subset of the data.
-    class SectionView < Plans::ApplicationRecord
+    class SectionViewBlock < Plans::ApplicationRecord
       include Decidim::HasComponent
     end
   end
