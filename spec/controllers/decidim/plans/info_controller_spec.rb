a9ef92f402b19273d22ac5abaee47a08d1f1ba12
Sina Eftekhar <sina.eftekhar@mainiotech.fi>
2023-11-15 11:02:53 +0200

    Add test for info controller

---
+++ spec/controllers/decidim/plans/info_controller_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Decidim::Plans::InfoController, type: :controller do
+  routes { Decidim::Plans::Engine.routes }
+
+  let(:component) { create(:plan_component) }
+  let(:participatory_space) { component.participatory_space }
+  let!(:plan) { create(:plan, component: component) }
+
+  before do
+    request.env["decidim.current_organization"] = component.organization
+    request.env["decidim.current_participatory_space"] = participatory_space
+    request.env["decidim.current_component"] = component
+  end
+
+  it "raises error when section does not exist" do
+    expect { get :show, params: { section: 123, component_id: component.id } }
+      .to raise_error(ActionController::RoutingError)
+  end
+
+  context "with an available section" do
+    let!(:section) { create(:section, :field_text, component: component) }
+
+    it "shows section" do
+      expect { get :show, params: { section: section.id, component_id: component.id } }
+        .not_to raise_error
+      expect(response).to render_template(:show)
+      puts translated(section.information).inspect
+      expect(response.headers["X-Robots-Tag"]).to eq("none")
+    end
+  end
+end
